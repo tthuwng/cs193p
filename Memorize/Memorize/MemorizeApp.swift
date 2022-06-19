40afcc007cc097d4895fa31ba4ab19b9c8205b2c
hungtra <hungtra@tesla.com>
2022-06-19 15:25:38 -0700

    init

---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Hung Tran on 12/30/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
